a118ed117d1ce20ea0dea6ddb42bdca29bd5be9f
KawanoRiku0 <riku.kawano20020518@gmail.com>
2022-10-09 00:05:51 +0900

    feat: 都道府県一覧を表示する複数のcheckboxを表示するコンポーネントを作成

---
+++ src/components/pages/index/PrefecturesChecBoxes.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { Prefecture } from '../../../types/resasApi';
+import PrefectureCheckBox from './PrefectureCheckBox.vue';
+
+defineProps<{ prefectures: Prefecture[] }>()
+</script>
+
+<template>
+  <div>
+    <h2>都道府県</h2>
+
+    <div class="checkboxes-wapper">
+      <PrefectureCheckBox v-for="pre in prefectures" :key="pre?.prefCode" :prefecture="pre" />
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.checkboxes-wapper {
+  max-width: 75rem;
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
