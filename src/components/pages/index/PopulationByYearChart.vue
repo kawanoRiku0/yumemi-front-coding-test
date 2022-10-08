07314cd1b8e7b4aace33411737933f421d0a8932
KawanoRiku0 <riku.kawano20020518@gmail.com>
2022-10-08 23:54:42 +0900

    feat: グラフを表示するコンポーネントを作成

---
+++ src/components/pages/index/PopulationByYearChart.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { Chart, ChartData, registerables } from "chart.js";
+import { LineChart } from "vue-chart-3"
+Chart.register(...registerables);
+
+defineProps<{ lineData: ChartData<"line"> }>()
+
+const options = {
+  scales: {
+    x: {
+      title: {
+        display: true,
+        text: '年度'
+      },
+    },
+    y: {
+      title: {
+        display: true,
+        text: '人口数'
+      }
+    }
+  }
+}
+
+
+</script>
+  
+<template>
+  <div>
+    <h2>総人口-年度グラフ</h2>
+    <LineChart :chart-data="lineData" :options="options" chart-id="population" />
+  </div>
+</template>
+  
+<style scoped>
+h2 {
+  text-align: left;
+}
+</style>
