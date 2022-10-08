bd29238bbc5e68861adc87ca7e3c9481a907a18f
KawanoRiku0 <riku.kawano20020518@gmail.com>
2022-10-09 00:05:17 +0900

    feat: 都道府県を表示するcheckboxコンポーネントを作成

---
+++ src/components/pages/index/PrefectureCheckBox.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Prefecture } from '../../../types/resasApi';
+import CheckBoxButton from './CheckBoxButton.vue';
+
+defineProps<{ prefecture: Prefecture }>()
+
+const isChecked = ref(false)
+const setIsChecked = (state: boolean) => {
+  isChecked.value = state
+}
+
+</script>
+
+<template>
+  <CheckBoxButton :label="prefecture.prefName" :state="isChecked" @on-state-change="setIsChecked" />
+</template>
+
+
