1f9097b95d6dacf9858f6515fa715ed91001ea85
KawanoRiku0 <riku.kawano20020518@gmail.com>
2022-10-08 23:35:42 +0900

    feat: Indexページへのルーティングを設定

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import IndexPage from "./pages/IndexPage.vue";
+
+const path = location.pathname
+
+const routes = {
+  indexPage: "/",
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="main">
+    <IndexPage v-if="path === routes.indexPage" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.main {
+  overflow-x: hidden;
 }
 </style>
