6853b020b74187d029bc3e1e3600fa3e99e18fd6
KawanoRiku0 <riku.kawano20020518@gmail.com>
2022-10-09 00:10:05 +0900

    feat: 人口構成取得に使用する型を定義

--- src/types/resasApi.ts
+++ src/types/resasApi.ts
@@ -3,7 +3,26 @@ export type GetPrefectures = {
   result: Prefecture[];
 };
 
+export type GetDemographics = {
+  message: string | null;
+  result: Demographic;
+};
+
+export type Demographic = {
+  boundaryYear: number;
+  data: {
+    label: string;
+    data: Population[];
+  }[];
+};
+
 export type Prefecture = {
   prefCode: number;
   prefName: string;
+};
+
+export type Population = {
+  year: number;
+  value: number;
+  rate?: number;
 };
